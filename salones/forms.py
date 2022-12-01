d81228108d5be26b3cd5878eabc5d53d16a8afa2
Raúl Pérez Durán <ghoulrul@gmail.com>
2022-12-01 12:19:34 -0600

    Se agrego el campo activo en los formularios

--- salones/forms.py
+++ salones/forms.py
@@ -4,17 +4,17 @@ from salones.models import TipoActividad, TipoEvento, TipoServicio
 class ActividadForm(forms.ModelForm):
     class Meta:
         model = TipoActividad
-        fields = ('cvetipoactividad', 'desctipoactividad')
+        fields = ('cvetipoactividad', 'desctipoactividad', 'bandactivo')
 
 class EventoForm(forms.ModelForm):
     class Meta:
         model = TipoEvento
-        fields = ('cvetipoevento', 'desctipoevento')
+        fields = ('cvetipoevento', 'desctipoevento', 'bandactivo')
 
 class ServicioForm(forms.ModelForm):
     class Meta:
         model = TipoServicio
-        fields = ('cvetiposervicio', 'descservicio')
+        fields = ('cvetiposervicio', 'descservicio', 'bandactivo')
 
 #from django.forms import inlineformset_factory
 #SalonFormSet = inlineformset_factory(Salon, Direccion, fields=('nombre', 'tipo', 'servicios',))
