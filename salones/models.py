ba5ea6281ff866606e138d1d8b9e5d194bce5136
Raúl Pérez Durán <ghoulrul@gmail.com>
2022-12-09 11:06:39 -0600

    Se unificaron los migrates y se agrego campo de busqueda a la lista de los catalogos

--- salones/models.py
+++ salones/models.py
@@ -3,7 +3,7 @@ class TipoActividad(models.Model):
     id_pk = models.SmallAutoField(db_column='IdTipoActividad', primary_key=True)  # Field name made lowercase.
     clave = models.CharField(db_column='CveTipoActividad', max_length=20,  verbose_name='Clave')  # Field name made lowercase.
     descripcion = models.CharField(db_column='DescTipoActividad', max_length=50, verbose_name='Descripcion')  # Field name made lowercase.
-    bandactivo = models.BooleanField(db_column='BandActivo', blank=True, null=True, default=True, verbose_name='Activo')  # Field name made lowercase.
+    bandactivo = models.BooleanField(db_column='BandActivo', blank=False, null=False, default=True, verbose_name='Activo')  # Field name made lowercase.
 
     def __str__(self) -> str:
         return self.clave
@@ -16,7 +16,7 @@ class TipoEvento(models.Model):
     id_pk = models.SmallAutoField(db_column='IdTipoEvento', primary_key=True)  # Field name made lowercase.
     clave = models.CharField(db_column='CveTipoEvento', max_length=20,  verbose_name='Clave', help_text='La clave que se mostrara en menus')  # Field name made lowercase.
     descripcion = models.CharField(db_column='DescTipoEvento', max_length=50, verbose_name='Descripcion')  # Field name made lowercase.
-    bandactivo = models.BooleanField(db_column='BandActivo', blank=True, null=True, default=True, verbose_name='Activo')  # Field name made lowercase.
+    bandactivo = models.BooleanField(db_column='BandActivo', blank=False, null=False, default=True, verbose_name='Activo')  # Field name made lowercase.
 
     def __str__(self) -> str:
         return self.clave
@@ -29,7 +29,7 @@ class TipoServicio(models.Model):
     id_pk = models.SmallAutoField(db_column='IdTipoServicio', primary_key=True)  # Field name made lowercase.
     clave = models.CharField(db_column='CveTipoServicio', max_length=20,  verbose_name='Clave')  # Field name made lowercase.
     descripcion = models.CharField(db_column='DescServicio', max_length=50, verbose_name='Descripcion')  # Field name made lowercase.
-    bandactivo = models.BooleanField(db_column='BandActivo', blank=True, null=True, default=True, verbose_name='Activo')  # Field name made lowercase.
+    bandactivo = models.BooleanField(db_column='BandActivo', blank=False, null=False, default=True, verbose_name='Activo')  # Field name made lowercase.
 
     def __str__(self) -> str:
         return self.clave
