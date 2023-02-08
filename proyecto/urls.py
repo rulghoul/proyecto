470cf237d46e2298c5f78222add394fdbaacc2cb
Raul Perez <ghoulrul@gmail.com>
2023-02-07 21:37:50 -0600

    color de pleca

--- proyecto/urls.py
+++ proyecto/urls.py
@@ -3,33 +3,16 @@ from django.urls import path, include
 
 from django.views.generic.base import TemplateView
 from salones.views import (
-    user_login, 
-    add_actividad, ActividadViewSet, add_evento, EventoViewSet, 
-    add_servicio, ServicioViewSet, 
-    lista_evento, 
-    update_evento, 
-    lista_actividad, 
-    update_actividad, 
-    lista_servicio, 
-    update_servicio, 
-    home_view, 
-    detalle_actividad, 
-    detalle_evento, 
-    detalle_servicio,
-    EventoCreateView,
-    ActividadCreateView,
-    ServicioCreateView,
-    eventos,
-    add_evento_completo,
-    update_evento_completo,
-    personas,
-    add_cliente,
-    update_cliente,
-    lista_tipo_cliente,
-    lista_tipo_persona,
-    add_tipo_cliente,
-    update_tipo_cliente,
-    )
+    user_login,           
+    home_view,
+    lista_actividad, add_actividad, update_actividad, ActividadViewSet,
+    lista_evento, add_evento, update_evento, EventoViewSet,
+    lista_servicio, add_servicio,  update_servicio, ServicioViewSet,
+    eventos, add_evento_completo, update_evento_completo,
+    lista_tipo_persona, add_tipo_cliente, update_tipo_cliente,
+    personas, add_cliente, update_cliente,
+)
+
 from django.contrib.auth import views as auth_views
 
 from rest_framework import routers
@@ -38,7 +21,45 @@ router.register(r'actividades', ActividadViewSet)
 router.register(r'eventos', EventoViewSet)
 router.register(r'servicios', ServicioViewSet)
 
+path_tipo_actividades = [
+    path('lista_actividad', lista_actividad, name='lista_actividad'),
+    path('add_actividad/', add_actividad, name='add_salon'),
+    path('actividad/<int:id_actividad>/', update_actividad, name='update_actividad')
+]
+
+path_tipo_eventos = [
+    path('lista_evento', lista_evento.as_view(), name='lista_evento'),
+    path('add_evento/', add_evento, name='add_evento'),
+    path('evento/<int:pk>/', update_evento.as_view(), name='update_evento'),
+]
+
+path_tipo_servicios = [
+    path('lista_servicio', lista_servicio, name='lista_servicio'),
+    path('add_servicio/', add_servicio, name='add_servicio'),
+    path('servicio/<int:id_servicio>/', update_servicio, name='update_servicio')  
+]
+
+path_tipo_personas = [
+    path('lista_tipo_cliente', lista_tipo_persona.as_view(), name='lista_tipo_cliente'),
+    path('add_tipo_cliente', add_tipo_cliente, name='add_tipo_cliente'),
+    path('update_tipo_cliente/<int:id_tipo_persona>/', update_tipo_cliente, name='update_tipo_cliente')
+]
+
+path_personas = [
+    path('clientes', personas.as_view(), name='clientes'),
+    path('add_cliente', add_cliente, name='add_cliente'),
+    path('update_cliente/<int:id_persona>/', update_cliente, name='update_cliente')
+]
+
+path_eventos = [
+    path('eventos', eventos.as_view(), name='eventos'),
+    path('add_evento_completo', add_evento_completo, name='add_evento_completo'),
+    path('update_evento_completo/<int:id_evento>/', update_evento_completo, name='update_evento_completo')
+]
+
 urlpatterns = [
+
+    path('', home_view, name='home'),    
     path('login/', user_login, name='user_login'),
     path('change-password/', auth_views.PasswordChangeView.as_view()),
     path('admin/', admin.site.urls),
@@ -47,30 +68,10 @@ urlpatterns = [
     path('json/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     #termina la parte rest
-    path('', home_view, name='home'), 
-    #path('add_salon/', add_salon, name='add_salon'),
-    path('add_actividad/', add_actividad, name='add_salon'),
-    path('add_evento/', add_evento, name='add_evento'),
-    path('add_servicio/', add_servicio, name='add_servicio'),
-    path('lista_actividad', lista_actividad, name='lista_actividad'),
-    path('lista_evento', lista_evento.as_view(), name='lista_evento'),
-    path('lista_servicio', lista_servicio, name='lista_servicio'),
-    path('actividad/<int:id_actividad>/', update_actividad, name='update_actividad'),
-    path('evento/<int:id_evento>/', update_evento, name='update_evento'),
-    path('servicio/<int:id_servicio>/', update_servicio, name='update_servicio'),    
-    path('actividad_detalle/<int:id_actividad>/', detalle_actividad, name='detalle_actividad'),
-    path('evento_detalle/<int:id_evento>/', detalle_evento, name='detalle_evento'),
-    path('servicio_detalle/<int:id_servicio>/', detalle_servicio, name='detalle_servicio'),
-    path('add_accion_modal/', ActividadCreateView.as_view(), name='add_accion_modal'),
-    path('add_evento_modal/', EventoCreateView.as_view(), name='add_evento_modal'),
-    path('add_evento_modal/', ServicioCreateView.as_view(), name='add_servicio_modal'),
-    path('eventos', eventos.as_view(), name='eventos'),
-    path('add_evento_completo', add_evento_completo, name='add_evento_completo'),
-    path('update_evento_completo/<int:id_evento>/', update_evento_completo, name='update_evento_completo'),
-    path('clientes', personas.as_view(), name='clientes'),
-    path('add_cliente', add_cliente, name='add_cliente'),
-    path('update_cliente/<int:id_persona>/', update_cliente, name='update_cliente'),
-    path('lista_tipo_cliente', lista_tipo_persona.as_view(), name='lista_tipo_cliente'),
-    path('add_tipo_cliente', add_tipo_cliente, name='add_tipo_cliente'),
-    path('update_tipo_cliente/<int:id_tipo_persona>/', update_tipo_cliente, name='update_tipo_cliente'),
-]
+
+] + path_tipo_actividades  \
++ path_tipo_eventos \
++ path_tipo_servicios \
++ path_tipo_personas \
++ path_personas \
++ path_eventos #\
--- salones/views.py
+++ salones/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth import authenticate, login
 from django.views.generic.edit import CreateView
 from django.views import generic
-from django.urls import reverse
+from django.urls import reverse, reverse_lazy
 
 from salones.models import (
     TipoActividad, 
@@ -23,157 +23,6 @@ from .forms import (
     PersonaPrincipalForm,
     TipoClienteForm,
     )
-
-class ActividadCreateView(CreateView):
-    model = TipoActividad
-    fields = ('clave', 'descripcion')
-
-class EventoCreateView(CreateView):
-    model = TipoEvento
-    fields = ('clave', 'descripcion')
-
-class ServicioCreateView(CreateView):
-    model = TipoServicio
-    fields = ('clave', 'descripcion')
-
-def add_actividad(request):
-    form = ActividadForm(request.POST or None)
-    if form.is_valid():
-        instance = form.save(commit=False)
-        instance.save() 
-        return redirect(lista_actividad)    
-    template = loader.get_template('salones/catalogos/add.html')
-    context = {'titulo': "Nueva Actividad", "form": form, 'regresa':'lista_actividad'}
-    return HttpResponse(template.render(context, request))
-
-def update_actividad(request, id_actividad):
-    actividad = get_object_or_404(TipoActividad, pk=id_actividad)
-    if request.method == 'POST':
-        form = EventoForm(request.POST,instance=actividad)
-        if form.is_valid():
-            form.save()
-            return redirect(lista_actividad)
-    else:
-        form = EventoForm(instance=actividad)
-    template = loader.get_template('salones/catalogos/update.html')
-    context = {'catalogo': actividad, 'titulo': "Actualiza Actividad", "form": form, 'regresa':'lista_actividad'}
-    return HttpResponse(template.render(context, request))
-
-def add_evento(request):
-    form = EventoForm(request.POST or None)
-    if form.is_valid():
-        instance = form.save(commit=False)
-        instance.save() 
-        return redirect(lista_evento)    
-    template = loader.get_template('salones/catalogos/add.html')
-    context = {'titulo': "Nuevo Evento", "form": form, 'regresa':'lista_evento'}
-    return HttpResponse(template.render(context, request))
-
-
-def update_evento(request, id_evento):
-    evento = get_object_or_404(TipoEvento, pk=id_evento)
-    if request.method == 'POST':
-        form = EventoForm(request.POST,instance=evento)
-        if form.is_valid():
-            form.save()
-            return redirect(lista_evento)
-    else:
-        form = EventoForm(instance=evento)
-    template = loader.get_template('salones/catalogos/update.html')
-    context = {'catalogo': evento, 'titulo': "Actualiza Evento", "form": form, 'regresa':'lista_evento'}
-    return HttpResponse(template.render(context, request))
-
-def add_servicio(request):
-    form = ServicioForm(request.POST or None)
-    if form.is_valid():
-        instance = form.save(commit=False)
-        instance.save()
-        return redirect(lista_servicio)    
-    template = loader.get_template('salones/catalogos/add.html')
-    context = {'titulo': "Nuevo Servicio", "form": form, 'regresa':'lista_servicio'}
-    return HttpResponse(template.render(context, request))   
-
-def update_servicio(request, id_servicio):
-    servicio = get_object_or_404(TipoServicio, pk=id_servicio)
-    if request.method == 'POST':
-        form = EventoForm(request.POST,instance=servicio)
-        if form.is_valid():
-            form.save()
-            return redirect(lista_servicio)
-    else:
-        form = ServicioForm(instance=servicio)
-    template = loader.get_template('salones/catalogos/update.html')
-    context = {'catalogo': servicio, 'titulo': "Actualiza Servicio", "form": form, 'regresa':'lista_servicio'}
-    return HttpResponse(template.render(context, request))
-
-from rest_framework import viewsets
-from rest_framework import permissions
-from .serializers import ActividadSerializer, EventoSerializer, ServicioSerializer
-
-
-class ActividadViewSet(viewsets.ModelViewSet):
-    queryset = TipoActividad.objects.all()
-    serializer_class = ActividadSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class EventoViewSet(viewsets.ModelViewSet):
-    queryset = TipoEvento.objects.all()
-    serializer_class = EventoSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-class ServicioViewSet(viewsets.ModelViewSet):
-    queryset = TipoServicio.objects.all()
-    serializer_class = ServicioSerializer
-    permission_classes = [permissions.IsAuthenticated]
-
-def lista_actividad(request):
-    actividad_list = TipoActividad.objects.all().order_by('clave').values()
-    template = loader.get_template('salones/catalogos/list.html')
-    context = {
-        'lista': actividad_list,
-        'titulo': "Actividades",
-        'add':"add_actividad",
-        'add_label':'Nueva actividad',
-        'update':'update_actividad',
-        'detalle':'detalle_actividad',
-        'encabezados': {"id":'ID',"clave":"CLAVE","descripcion":"DESCRIPCION", "activo":"ACTIVO"},
-    }
-    return HttpResponse(template.render(context, request))
-
-class lista_evento(generic.ListView):
-    model = TipoEvento
-    template_name = ('salones/catalogos/list.html')
-
-    def get_context_data(self, **kwargs):
-        # Call the base implementation first to get a context
-        context = super().get_context_data(**kwargs)
-        # Add in a QuerySet of all the books
-        context['lista'] = TipoEvento.objects.all().order_by('clave').values()
-        datos = {
-            'titulo': "Eventos",
-            'add':"add_evento",
-            'add_label':'Nuevo Evento',
-            'update':'update_evento',        
-            'detalle':'detalle_evento',
-            'encabezados': {"id":'Folio',"clav e":"CLAVE","descripcion":"DESCRIPCION", "activo":"ACTIVO"},
-        }
-        context.update(datos)
-        return context
-    
-def lista_servicio(request):
-    servicio_list = TipoServicio.objects.all().order_by('clave').values()
-    template = loader.get_template('salones/catalogos/list.html')
-    context = {
-        'lista': servicio_list,
-        'titulo': "Servicios",
-        'add':"add_servicio",
-        'add_label':'Nuevo Servicio',
-        'update':'update_servicio',
-        'detalle':'detalle_servicio',
-        'encabezados': {"id":'ID',"clave":"CLAVE","descripcion":"DESCRIPCION", "activo":"ACTIVO"},
-    }
-    return HttpResponse(template.render(context, request))
-
 
 
 def user_login(request):
@@ -200,6 +49,166 @@ def home_view(request):
     fotos = Foto.objects.all()
     context = {'titulo': "Nueva Actividad", "fotos": fotos, 'regresa':'lista_actividad'}
     return HttpResponse(template.render(context, request))
+
+#Catalogos
+
+def add_actividad(request):
+    form = ActividadForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save() 
+        return redirect(lista_actividad)    
+    template = loader.get_template('salones/catalogos/add.html')
+    context = {'titulo': "Nueva Actividad", "form": form, 'regresa':'lista_actividad'}
+    return HttpResponse(template.render(context, request))
+
+def update_actividad(request, id_actividad):
+    actividad = get_object_or_404(TipoActividad, pk=id_actividad)
+    if request.method == 'POST':
+        form = EventoForm(request.POST,instance=actividad)
+        if form.is_valid():
+            form.save()
+            return redirect(lista_actividad)
+    else:
+        form = EventoForm(instance=actividad)
+    template = loader.get_template('salones/catalogos/update.html')
+    context = {'catalogo': actividad, 'titulo': "Actualiza Actividad", "form": form, 'regresa':'lista_actividad'}
+    return HttpResponse(template.render(context, request))
+
+def lista_actividad(request):
+    actividad_list = TipoActividad.objects.all().order_by('clave').values()
+    template = loader.get_template('salones/catalogos/list.html')
+    context = {
+        'lista': actividad_list,
+        'titulo': "Actividades",
+        'add':"add_actividad",
+        'add_label':'Nueva actividad',
+        'update':'update_actividad',
+        'detalle':'detalle_actividad',
+        'encabezados': {"id":'ID',"clave":"CLAVE","descripcion":"DESCRIPCION", "activo":"ACTIVO"},
+    }
+    return HttpResponse(template.render(context, request))
+
+
+  
+def add_evento(request):
+    form = EventoForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save() 
+        return redirect(lista_evento)    
+    template = loader.get_template('salones/catalogos/add.html')
+    context = {'titulo': "Nuevo Evento", "form": form, 'regresa':'lista_evento'}
+    return HttpResponse(template.render(context, request))
+
+class update_evento(generic.edit.UpdateView):
+    model = TipoEvento
+    fields = ['clave','descripcion','bandactivo']
+    success_url = reverse_lazy('lista_evento')
+    template_name = 'salones/catalogos/update.html'
+    
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        context['titulo'] = "Actualiza Evento"
+        context['regresa'] = 'lista_evento'
+        return context
+
+
+# def update_evento(request, id_evento):
+#     evento = get_object_or_404(TipoEvento, pk=id_evento)
+#     if request.method == 'POST':
+#         form = EventoForm(request.POST,instance=evento)
+#         if form.is_valid():
+#             form.save()
+#             return redirect(lista_evento)
+#     else:
+#         form = EventoForm(instance=evento)
+#     template = loader.get_template('salones/catalogos/update.html')
+#     context = {'catalogo': evento, 'titulo': "Actualiza Evento", "form": form, 'regresa':'lista_evento'}
+#     return HttpResponse(template.render(context, request))
+
+
+class lista_evento(generic.ListView):
+    model = TipoEvento
+    template_name = ('salones/catalogos/list.html')
+
+    def get_context_data(self, **kwargs):
+        # Call the base implementation first to get a context
+        context = super().get_context_data(**kwargs)
+        # Add in a QuerySet of all the books
+        context['lista'] = TipoEvento.objects.all().order_by('clave').values()
+        datos = {
+            'titulo': "Eventos",
+            'add':"add_evento",
+            'add_label':'Nuevo Evento',
+            'update':'update_evento',        
+            'detalle':'detalle_evento',
+            'encabezados': {"id":'Folio',"clav e":"CLAVE","descripcion":"DESCRIPCION", "activo":"ACTIVO"},
+        }
+        context.update(datos)
+        return context
+    
+
+
+def add_servicio(request):
+    form = ServicioForm(request.POST or None)
+    if form.is_valid():
+        instance = form.save(commit=False)
+        instance.save()
+        return redirect(lista_servicio)    
+    template = loader.get_template('salones/catalogos/add.html')
+    context = {'titulo': "Nuevo Servicio", "form": form, 'regresa':'lista_servicio'}
+    return HttpResponse(template.render(context, request))   
+
+def update_servicio(request, id_servicio):
+    servicio = get_object_or_404(TipoServicio, pk=id_servicio)
+    if request.method == 'POST':
+        form = EventoForm(request.POST,instance=servicio)
+        if form.is_valid():
+            form.save()
+            return redirect(lista_servicio)
+    else:
+        form = ServicioForm(instance=servicio)
+    template = loader.get_template('salones/catalogos/update.html')
+    context = {'catalogo': servicio, 'titulo': "Actualiza Servicio", "form": form, 'regresa':'lista_servicio'}
+    return HttpResponse(template.render(context, request))
+
+def lista_servicio(request):
+    servicio_list = TipoServicio.objects.all().order_by('clave').values()
+    template = loader.get_template('salones/catalogos/list.html')
+    context = {
+        'lista': servicio_list,
+        'titulo': "Servicios",
+        'add':"add_servicio",
+        'add_label':'Nuevo Servicio',
+        'update':'update_servicio',
+        'detalle':'detalle_servicio',
+        'encabezados': {"id":'ID',"clave":"CLAVE","descripcion":"DESCRIPCION", "activo":"ACTIVO"},
+    }
+    return HttpResponse(template.render(context, request))
+
+
+from rest_framework import viewsets
+from rest_framework import permissions
+from .serializers import ActividadSerializer, EventoSerializer, ServicioSerializer
+
+
+class ActividadViewSet(viewsets.ModelViewSet):
+    queryset = TipoActividad.objects.all()
+    serializer_class = ActividadSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class EventoViewSet(viewsets.ModelViewSet):
+    queryset = TipoEvento.objects.all()
+    serializer_class = EventoSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+class ServicioViewSet(viewsets.ModelViewSet):
+    queryset = TipoServicio.objects.all()
+    serializer_class = ServicioSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
 
 
 class eventos(generic.ListView):
@@ -245,7 +254,7 @@ def add_evento_completo(request):
         instance.opcion = 1 #Agrega la opcion 1 a la instancia
         instance.save() 
         #return redirect(eventos)
-        return  redirect(reverse('detail_evento', kwargs={'evento_id': instance.id}))   
+        return  redirect(reverse('update_evento_completo', kwargs={'evento_id': instance.id}))   
     template = loader.get_template('salones/catalogos/add.html')
     context = {'titulo': "Nuevo Evento", "form": form, 'regresa':'eventos'}
     return HttpResponse(template.render(context, request))
@@ -264,9 +273,6 @@ def update_evento_completo(request, id_evento):
     return HttpResponse(template.render(context, request))
 
 
-
-
-from django.urls import reverse_lazy
 
 def detalle_actividad(request, id_ctividad):
     evento = get_object_or_404(TipoActividad, pk=id_ctividad)
